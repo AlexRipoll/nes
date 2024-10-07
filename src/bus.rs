9df4f1f3de7af8d12e751081f442464a01ad7b57
Alex Ripoll <alexripollpiera@gmail.com>
2024-10-07 20:05:48 +0200

    move mem unit tests to bus module

--- src/bus.rs
+++ src/bus.rs
@@ -53,4 +53,55 @@ impl Bus {
             }
         }
     }
+
+    /// Reads two bytes from memory at the specified `address`, returning a 16-bit value.
+    /// This is little-endian, meaning the least significant byte is read first.
+    pub fn mem_read_u16(&self, address: u16) -> u16 {
+        let lsb = self.mem_read(address) as u16;
+        let msb = self.mem_read(address + 1) as u16;
+        (msb << 8) | (lsb as u16)
+    }
+
+    /// Writes a 16-bit value to memory at the specified `address`.
+    pub fn mem_write_u16(&mut self, address: u16, data: u16) {
+        let msb = (data >> 8) as u8;
+        let lsb = (data & 0xff) as u8;
+        self.mem_write(address, lsb);
+        self.mem_write(address + 1, msb);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::bus::Bus;
+
+    #[test]
+    fn test_mem_read() {
+        let mut bus = Bus::new();
+        bus.cpu_vram[0x12] = 100;
+        assert_eq!(bus.mem_read(0x12), 100);
+    }
+
+    #[test]
+    fn test_mem_write() {
+        let mut bus = Bus::new();
+        bus.mem_write(0x12, 100);
+        assert_eq!(bus.cpu_vram[0x12], 100);
+    }
+
+    #[test]
+    fn test_mem_read_u16() {
+        let mut bus = Bus::new();
+        bus.cpu_vram[0xab] = 0x10;
+        bus.cpu_vram[0xac] = 0x11;
+        assert_eq!(bus.mem_read_u16(0xab), 0x1110);
+    }
+
+    #[test]
+    fn test_mem_write_u16() {
+        let mut bus = Bus::new();
+        bus.mem_write_u16(0xab, 0x10ab);
+        assert_eq!(bus.cpu_vram[0xab], 0xab);
+        assert_eq!(bus.cpu_vram[0xac], 0x10);
+    }
 }
