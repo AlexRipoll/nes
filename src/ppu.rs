24db7e5749220cf222f3c32135db0a21fb53396c
Alex Ripoll <alexripollpiera@gmail.com>
2024-10-23 19:31:21 +0200

    moving PPU registers files to parent directory

--- src/ppu.rs
+++ src/ppu.rs
@@ -1,2 +1,7 @@
+pub mod address;
+pub mod controller;
+pub mod mask;
 pub mod ppu;
-pub mod register;
+pub mod scroll;
+pub mod status;
+pub mod vram;
--- src/ppu/register.rs
+++
@@ -1,5 +0,0 @@
-pub mod address;
-pub mod controller;
-pub mod mask;
-pub mod scroll;
-pub mod status;
